3fa732c535c384d48ee95a6e7a6c69b7ca51d876
Yong Li <2282147975@qq.com>
2020-09-08 17:30:31 +0800

    Re_calc added

---
+++ functions/Reynolds.cpp
@@ -0,0 +1,36 @@
+// calculate Reynold number
+// Re=rho*U*D/mu
+#include<iostream>
+#include<string>
+#include<cmath>
+
+using namespace std;
+double Reynolds_calc(double rho, double U, double D, double mu);
+
+int main()
+{
+    double rho,U,D,mu;
+    cout << "We are now going to calculate the Reynold number:" << endl;
+
+    cout << "Please input density in kg/m3" << endl;
+    cin >> rho;
+
+    cout << "Please input velocity in m/s" << endl;
+    cin >> U;
+
+    cout << "Please input characteristic length in m" << endl;
+    cin >> D;
+
+    cout << "Please input dynamic viscosity in SI units" << endl;
+    cin >> mu;
+
+    // calc Re
+    cout << "Re = " << Reynolds_calc(rho,U,D,mu) << endl;
+    return 0;
+}
+
+double Reynolds_calc(double rho, double U, double D, double mu)
+{
+    double Re=rho*U*D/mu;
+    return Re;
+}
